3110d9345678fb5b52f291c80af2c5fb2102948f
Краснов Александр Григорьевич <211916@mck-ktits.ru>
2021-06-18 14:26:52 +0300

    Добавьте файлы проекта.

---
+++ KrasnovWiFi_Bot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelegramBotClient bot = new TelegramBotClient("1863063161:AAHJIZjhYb_U3prsRfgF1KCDse4aub0lb20");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Бот говорит: {arg.Message.Text}");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
